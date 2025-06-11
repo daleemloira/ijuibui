7780a64018a2c029c0158c7466ffdc81a8ebe63c
daleemloira <vitor.prates.ferreira@escola.pr.gov.br>
2025-06-11 12:02:13 -0300

    vitin

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Futebol',
+    'Quem é o maior artilheiro da seleção brasileira?'1  ,
+    'Maior artileiro da seleção brasileira é o Neymar'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Futebol',
+    'Em que time Neymar joga?',
+    'Atualmente Neymar joga no Santos'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Futebol',
+    'Que time foi campeão da Champions de 2015?',
+    'O time campeão da Champions de 2015 foi o Barcelona'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'MUsica',
+    'o que significa EVOM?',
+    'Significa EU VENCI O MUNDO, que será um novo album do cantor veigh'
 )
